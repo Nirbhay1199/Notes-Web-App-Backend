dc04e08c2254d4150422891d49d0ad4b8a5aa794
Nirbhay1199 <nirbhay1199@gmail.com>
2025-09-01 15:37:52 +0530

    cros fix

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,8 +1,24 @@
 const cors = require('cors');
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://notes-f63bc8.netlify.app'
+];
+
 const corsConfig = {
-  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
-  credentials: true
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
 };
 
 module.exports = corsConfig;
